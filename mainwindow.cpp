963ffae74e1d03f38400304cb98e41e059183fe1
Kevin Chang <kevinjchang98@gmail.com>
2023-02-04 18:23:36 -0800

    minor reformat for constructor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,8 +14,7 @@
 #include "mainwindow.h"
 #include "ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QWidget(parent), ui(new Ui::MainWindow) {
+MainWindow::MainWindow(QWidget *parent) : QWidget(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
 
   // Window size
