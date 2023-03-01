5466537fece1dd8275ca72a0995200f13b796c2b
Kevin Chang <kevinjchang98@gmail.com>
2023-03-01 15:36:01 -0800

    run autoformat with updated max col settings

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,7 +17,8 @@
 #include "mainwindow.h"
 #include "ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent) : QWidget(parent), ui(new Ui::MainWindow) {
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
 
   connectionForm();
@@ -33,7 +34,10 @@ void MainWindow::routeForm() {
   // MUX Connection Label
   auto inputRoutingLabel = new QLabel(this);
   inputRoutingLabel->setText("Display Routing");
-  inputRoutingLabel->setGeometry(window_margin, window_margin + item_offset_vertical * 3, window_width, item_height);
+  inputRoutingLabel->setGeometry(window_margin,
+                                 window_margin + item_offset_vertical * 3,
+                                 window_width,
+                                 item_height);
 
   // Input routing boxes
   auto fromInputBox = new QSpinBox(this);
@@ -45,38 +49,48 @@ void MainWindow::routeForm() {
   fromInputBox->setMaximum(4);
   fromInputBox->setValue(1);
 
-  QObject::connect(fromInputBox, &QSpinBox::valueChanged, [this](auto newFrom) { from = newFrom; });
+  QObject::connect(fromInputBox,
+                   &QSpinBox::valueChanged,
+                   [this](auto newFrom) { from = newFrom; });
 
   auto toInputBox = new QSpinBox(this);
-  toInputBox->setGeometry(window_margin + 2 * (window_width / 4 - 4 * window_margin),
-                          window_margin + item_offset_vertical * 4,
-                          window_width / 4 - 4 * window_margin,
-                          item_height);
+  toInputBox->setGeometry(
+      window_margin + 2 * (window_width / 4 - 4 * window_margin),
+      window_margin + item_offset_vertical * 4,
+      window_width / 4 - 4 * window_margin,
+      item_height);
   toInputBox->setMinimum(1);
   toInputBox->setMaximum(4);
   toInputBox->setValue(1);
 
-  QObject::connect(toInputBox, &QSpinBox::valueChanged, [this](auto newTo) { to = newTo; });
+  QObject::connect(toInputBox,
+                   &QSpinBox::valueChanged,
+                   [this](auto newTo) { to = newTo; });
 
   // Button to set new route
   auto setRouteButton = new QPushButton("Set", this);
-  setRouteButton->setGeometry(window_margin + 4 * (window_width / 4 - 4 * window_margin),
-                              window_margin + item_offset_vertical * 4,
-                              50,
-                              item_height);
+  setRouteButton->setGeometry(
+      window_margin + 4 * (window_width / 4 - 4 * window_margin),
+      window_margin + item_offset_vertical * 4,
+      50,
+      item_height);
 
   // Run setRoute() on button press
   QObject::connect(setRouteButton, &QPushButton::pressed, [this]() {
     if (setRoute())
       statusText->setText(QString::fromStdString(
-          "Route updated to " + std::to_string(from) + " -> " + std::to_string(to)));
+          "Route updated to " + std::to_string(from) + " -> "
+              + std::to_string(to)));
     else
       statusText->setText("Error updating route");
   });
 
   // Button to get current route
   auto getRouteButton = new QPushButton("Get current route", this);
-  getRouteButton->setGeometry(window_margin, window_margin + item_offset_vertical * 5, 150, item_height);
+  getRouteButton->setGeometry(window_margin,
+                              window_margin + item_offset_vertical * 5,
+                              150,
+                              item_height);
 
   // Run getRoute() on button press
   QObject::connect(getRouteButton, &QPushButton::pressed, [this]() {
@@ -95,7 +109,10 @@ void MainWindow::connectionForm() {
   // MUX Connection Label
   auto connectionInputLabel = new QLabel(this);
   connectionInputLabel->setText("MUX Connection");
-  connectionInputLabel->setGeometry(window_margin, window_margin, window_width, item_height);
+  connectionInputLabel->setGeometry(window_margin,
+                                    window_margin,
+                                    window_width,
+                                    item_height);
 
   // Host input box
   auto hostInputBox = new QLineEdit(host, this);
@@ -106,21 +123,29 @@ void MainWindow::connectionForm() {
 
   // Port input box
   auto portInputBox = new QSpinBox(this);
-  portInputBox->setGeometry(window_margin + window_width / 2 - 2 * window_margin,
-                            window_margin + item_offset_vertical * 1,
-                            window_width / 2 - 4 * window_margin,
-                            item_height);
+  portInputBox->setGeometry(
+      window_margin + window_width / 2 - 2 * window_margin,
+      window_margin + item_offset_vertical * 1,
+      window_width / 2 - 4 * window_margin,
+      item_height);
   portInputBox->setMinimum(0);
   portInputBox->setMaximum(65535);
   portInputBox->setValue(5000);
 
   // Update host and port on value change
-  QObject::connect(hostInputBox, &QLineEdit::textEdited, [this](auto newHost) { host = newHost; });
-  QObject::connect(portInputBox, &QSpinBox::valueChanged, [this](auto newPort) { port = newPort; });
+  QObject::connect(hostInputBox,
+                   &QLineEdit::textEdited,
+                   [this](auto newHost) { host = newHost; });
+  QObject::connect(portInputBox,
+                   &QSpinBox::valueChanged,
+                   [this](auto newPort) { port = newPort; });
 
   // Button to connect to MUX
   auto connectButton = new QPushButton("Connect", this);
-  connectButton->setGeometry(10, window_margin + item_offset_vertical * 2, 80, item_height);
+  connectButton->setGeometry(10,
+                             window_margin + item_offset_vertical * 2,
+                             80,
+                             item_height);
 
   // Run connect() on button press
   QObject::connect(connectButton, &QPushButton::pressed, [this]() {
@@ -153,7 +178,8 @@ auto MainWindow::connect() -> bool {
 
   int attemptNumber = 0;
 
-  while (::connect(muxSocket, (sockaddr *) &sendSockAddr, sizeof(sendSockAddr)) < 0) {
+  while (::connect(muxSocket, (sockaddr *) &sendSockAddr, sizeof(sendSockAddr))
+      < 0) {
     muxSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (++attemptNumber == 11)
@@ -191,6 +217,7 @@ auto MainWindow::getRoute() const -> std::string {
     res.append(buffer, n);
   }
 
-  return std::string() + "Current routing: " + res[8] + "->" + res[6] + "  " + res[13] + "->" + res[11] + "  " + res[18]
-      + "->" + res[16] + "  " + res[23] + "->" + res[21];
+  return std::string() + "Current routing: " + res[8] + "->" + res[6] + "  "
+      + res[13] + "->" + res[11] + "  " + res[18] + "->" + res[16] + "  "
+      + res[23] + "->" + res[21];
 }
